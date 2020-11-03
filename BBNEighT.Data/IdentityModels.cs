0866e9b3917dcd539f5453397fceba99e13492ca
weswolff <weswolff@gmail.com>
2020-11-03 10:50:31 -0500

    testing wes push

--- BBNEighT.Data/IdentityModels.cs
+++ BBNEighT.Data/IdentityModels.cs
@@ -10,7 +10,8 @@ using Microsoft.AspNet.Identity.Owin;
 
 namespace BBNEighT.Data
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    // Adding comment for test push
+    //You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
