05171013295f005985cd96595bfee6297cbf9099
Antonio <jacr777@gmail.com>
2020-11-15 14:23:45 -0500

    item_fix

--- BBNEighT.Models/ItemModels/ItemListItem.cs
+++ BBNEighT.Models/ItemModels/ItemListItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,9 @@ namespace BBNEighT.Models.Item
         public int ItemID { get; set; }
         public string ItemName { get; set; }
         public int CategoryID { get; set; }
+        public string Category { get; set; }
         public int RoomID { get; set; }
+        public string Room { get; set; }
         public string Description { get; set; }
         public decimal Value { get; set; }
         public DateTime AquiredDate { get; set; }
